0f0caa156d094deb15ff81c1ea034199cb9942f7
krkms2000 <minsu0022@naver.com>
2023-12-15 13:43:16 +0900

    Initial version

---
+++ MyGit/src/project/B.java
@@ -0,0 +1,9 @@
+package project;
+
+public class B {
+
+	public static void main(String[] args) {
+		System.out.println("B java!");
+	}
+
+}
---
+++ MyGit/src/project/C.java
@@ -0,0 +1,10 @@
+package project;
+
+public class C {
+
+	public static void main(String[] args) {
+		System.out.println("C java!");
+
+	}
+
+}

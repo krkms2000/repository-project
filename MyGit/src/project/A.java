284b1d5b83b439374ffbcac5b537d8cf6d2e33d8
krkms2000 <minsu0022@naver.com>
2023-12-15 13:57:42 +0900

    change in devC

--- MyGit/src/project/A.java
+++ MyGit/src/project/A.java
@@ -5,6 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("A java!");
 		System.out.println("This is the 2nd version.");
+		System.out.println("My changed in devC branch");
 	}
 
 }
